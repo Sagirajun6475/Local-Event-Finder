40c3b0d057d2e97489a2758d5cf509ce231e8869
Sagirajun6475 <snvvarma4@gmail.com>
2024-09-24 15:28:53 -0500

    Initial Commit

---
+++ Local Event Finder/Local_Event_FinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Local_Event_FinderApp.swift
+//  Local Event Finder
+//
+//  Created by Nikhil Varma on 9/24/24.
+//
+
+import SwiftUI
+
+@main
+struct Local_Event_FinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Local Event FinderUITests/Local_Event_FinderUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Local_Event_FinderUITestsLaunchTests.swift
+//  Local Event FinderUITests
+//
+//  Created by Nikhil Varma on 9/24/24.
+//
+
+import XCTest
+
+final class Local_Event_FinderUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
